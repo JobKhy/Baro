850ef913af6e66499fe95507bc64af5d844c810d
jobKhy <pina.martinez.gael.job@gmail.com>
2022-11-25 18:19:35 -0600

    Slides

---
+++ client/src/components/Slides.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "../css/ETS.css"
+
+export const Slides = () => {
+  return (
+    <div className="SlAl">
+        <div className='Slides'>
+            <h1>¿Sabías que?</h1>
+            <p className="pslide">
+                Con baro puedes mantener un mejor control de tus gastos
+                y un registro acerca de estos
+            </p>
+        </div>
+        <div className='Slides'>
+            <h1>¿Sabías que?</h1>
+            <p className="pslide">
+                Con baro puedes mantener un mejor control de tus gastos
+                y un registro acerca de estos
+            </p>
+        </div>
+        <div className='Slides'>
+            <h1>¿Sabías que?</h1>
+            <p className="pslide">
+                Con baro puedes mantener un mejor control de tus gastos
+                y un registro acerca de estos
+            </p>
+        </div>
+    </div>
+    
+  )
+}
